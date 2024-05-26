3932a65dcadd1009ded17e3baf8a11ac73d2f0dc
Giancca <114377978+Giancardonee@users.noreply.github.com>
2024-05-26 20:31:33 -0300

    Add files via upload

---
+++ SGE.Repositorios/RepositorioExpedienteTXT.cs
@@ -0,0 +1,209 @@
+﻿using System.Numerics;
+using SGE.Aplicacion;
+
+namespace SGE.Repositorios;
+
+public class RepositorioExpedienteTXT : IExpedienteRepositorio
+{
+    static readonly string _nombreArch = "Expedientes.txt";
+    static int contadorExpedientes = contadorActual();
+
+    public void ExpedienteAlta(Expediente e)
+    {
+        try
+        {
+            ExpedienteValidador.Validar(e);
+            using var sw = new StreamWriter(_nombreArch, true);
+            e.IdExpediente = ++contadorExpedientes;
+            escribirValores(e, sw);
+        }
+        catch (ValidacionException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+        catch (Exception exc2)
+        {
+            Console.WriteLine(exc2);
+        }
+    }
+
+    public void ExpedienteBaja(int idBorrarExpediente)
+    {
+        try
+        {
+            bool encontre = false;
+            List<Expediente> listaExpediente = new List<Expediente>();
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Expediente eActual = leerExpediente(sr);
+                if (eActual.IdExpediente != idBorrarExpediente)
+                    listaExpediente.Add(eActual);
+                else
+                    encontre = true;
+            }
+            if (encontre)
+            {
+                sr.Dispose();
+                actualizarArchivo(listaExpediente);
+            }
+            else
+                throw new RepositorioException(
+                    "ExpedienteBaja: El expediente que se intenta eliminar no existe."
+                );
+        }
+        catch (RepositorioException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void ExpedienteModificacion(Expediente eModificar)
+    {
+        bool encontre = false;
+        List<Expediente> listaExpediente = new List<Expediente>();
+        try
+        {
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Expediente eActual = leerExpediente(sr);
+                if (eActual.IdExpediente == eModificar.IdExpediente)
+                {
+                    eActual.Caratula = eModificar.Caratula;
+                    eActual.Estado = eModificar.Estado;
+                    encontre = true;
+                }
+                listaExpediente.Add(eActual);
+            }
+            if (encontre)
+            {
+                sr.Dispose();
+                actualizarArchivo(listaExpediente);
+            }
+            else
+                throw new RepositorioException();
+        }
+        catch (RepositorioException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public Expediente? ExpedienteConsultaPorId(int IdExpediente)
+    {
+        try
+        {
+            Expediente resultado = new Expediente();
+            using var sr = new StreamReader(_nombreArch, true);
+            resultado = leerExpediente(sr);
+            while (!sr.EndOfStream && resultado.IdExpediente != IdExpediente)
+            {
+                resultado = leerExpediente(sr);
+            }
+            if (resultado.IdExpediente == IdExpediente)
+            {
+                return resultado;
+            }
+            else
+            {
+                throw new RepositorioException("ExpedienteConsultaPorId: el expediente no existe.");
+            }
+        }
+        catch (RepositorioException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
+    }
+
+    public List<Expediente> ExpedienteConsultaTodos()
+    {
+        List<Expediente> expedientes = new List<Expediente>();
+        try
+        {
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                expedientes.Add(leerExpediente(sr));
+            }
+            if (expedientes.Count == 0)
+                throw new RepositorioException();
+        }
+        catch (RepositorioException exp)
+        {
+            Console.WriteLine(exp.Message);
+        }
+        return expedientes;
+    }
+
+    //==================== metodos privados
+    private Expediente leerExpediente(StreamReader sr)
+    {
+        Expediente e = new Expediente();
+        string? id = sr.ReadLine();
+        if (!String.IsNullOrEmpty(id))
+        {
+            e.IdExpediente = int.Parse(id);
+            e.FechaCreacion = DateTime.Parse(sr.ReadLine() ?? "");
+            e.FechaModificacion = DateTime.Parse(sr.ReadLine() ?? "");
+            e.Caratula = sr.ReadLine() ?? "";
+            e.Estado = Enum.Parse<EstadoExpediente>(sr.ReadLine() ?? "");
+            e.UsuarioUltModificacion = int.Parse(sr.ReadLine() ?? "");
+        }
+        else
+        {
+            throw new RepositorioException("leerExpediente: El archivo está vacío");
+        }
+        return e;
+    }
+
+    private void actualizarArchivo(List<Expediente> listaExpedientes)
+    {
+        try
+        {
+            if (File.Exists(_nombreArch))
+            {
+                File.Delete(_nombreArch);
+                using var sw = new StreamWriter(_nombreArch, true);
+                foreach (Expediente e in listaExpedientes)
+                {
+                    escribirValores(e, sw);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error al actualizar el archivo. Error " + e.Message);
+        }
+    }
+
+    private static int contadorActual()
+    {
+        using var sr = new StreamReader(_nombreArch, true);
+        int ultimoId = 0;
+        while (!sr.EndOfStream)
+        {
+            string? id = sr.ReadLine();
+            if (!String.IsNullOrEmpty(id))
+            {
+                ultimoId = Int32.Parse(id);
+                for (int i = 1; i < 6; i++)
+                {
+                    sr.ReadLine();
+                }
+            }
+        }
+        return ultimoId;
+    }
+
+    private void escribirValores(Expediente e, StreamWriter sw)
+    {
+        sw.WriteLine(e.IdExpediente);
+        sw.WriteLine(e.FechaCreacion);
+        sw.WriteLine(e.FechaModificacion);
+        sw.WriteLine(e.Caratula);
+        sw.WriteLine(e.Estado);
+        sw.WriteLine(e.UsuarioUltModificacion);
+    }
+}
---
+++ SGE.Repositorios/RepositorioTramiteTxt.cs
@@ -0,0 +1,265 @@
+﻿using SGE.Aplicacion;
+
+namespace SGE.Repositorios;
+
+public class RepositorioTramiteTxt : ITramiteRepositorio
+{
+    static readonly string _nombreArch = "Tramites.txt";
+    static int contadorTramites = contadorActual();
+
+    public void TramiteAlta(Tramite t)
+    {
+        try
+        {
+            TramiteValidador.Validar(t);
+            using var sw = new StreamWriter(_nombreArch, true);
+            t.IdTramite = ++contadorTramites;
+            escribirValores(t, sw);
+        }
+        catch (ValidacionException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+    }
+
+    public void TramiteBaja(int idBorrarTramite)
+    {
+        bool encontre = false;
+        List<Tramite> listaTramite = new List<Tramite>();
+        try
+        {
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Tramite tActual = leerTramite(sr);
+                if (tActual.IdTramite != idBorrarTramite)
+                    listaTramite.Add(tActual);
+                else
+                    encontre = true;
+            }
+            if (encontre)
+            {
+                sr.Dispose();
+                actualizarArchivo(listaTramite);
+            }
+            else
+                throw new RepositorioException("El tramite que se intenta eliminar no existe.");
+        }
+        catch (RepositorioException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void TramiteModificacion(Tramite tModificar)
+    {
+        try
+        {
+            bool encontre = false;
+            List<Tramite> listaTramites = new List<Tramite>();
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Tramite tActual = leerTramite(sr);
+                if (tActual.IdTramite == tModificar.IdTramite)
+                {
+                    tActual.ExpedienteID = tModificar.ExpedienteID;
+                    tActual.FechaUltModificacion = DateTime.Now;
+                    tActual.TipoTramite = tModificar.TipoTramite;
+                    tActual.ContenidoTramite = tModificar.ContenidoTramite;
+                    tActual.UsuarioUltModificacion = tModificar.UsuarioUltModificacion;
+                    encontre = true;
+                }
+                listaTramites.Add(tActual);
+            }
+            if (encontre)
+            {
+                sr.Dispose();
+                actualizarArchivo(listaTramites);
+            }
+            else
+                throw new RepositorioException();
+        }
+        catch (RepositorioException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public List<Tramite> TramiteConsultaPorEtiqueta(EtiquetaTramite etiqueta)
+    {
+        List<Tramite> listaTramites = new List<Tramite>();
+        try
+        {
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Tramite tActual = leerTramite(sr);
+                if (tActual.TipoTramite == etiqueta)
+                {
+                    listaTramites.Add(tActual);
+                }
+            }
+            if (listaTramites.Count == 0)
+                throw new RepositorioException();
+        }
+        catch (RepositorioException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+
+        return listaTramites;
+    }
+
+    public List<Tramite> TramiteConsultaPorIdExpediente(int idExpediente)
+    {
+        List<Tramite> listaTramites = new List<Tramite>();
+        try
+        {
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Tramite tActual = leerTramite(sr);
+                if (tActual.ExpedienteID == idExpediente)
+                {
+                    listaTramites.Add(tActual);
+                }
+            }
+            if (listaTramites.Count == 0)
+                throw new RepositorioException("TramiteConsultaPorIdExpediente: el expediente no tiene trámites asociados.");
+        }
+        catch (RepositorioException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+        return listaTramites;
+    }
+
+    public Tramite TramiteConsultaUltimo()
+    {
+        Tramite t = new Tramite();
+        try
+        {
+            t.IdTramite = -1;
+            using var sr = new StreamReader(_nombreArch, true);
+
+            while (!sr.EndOfStream)
+            {
+                t = leerTramite(sr);
+            }
+        }
+        catch (RepositorioException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return t;
+    }
+
+    //==================== metodos privados
+    private void escribirValores(Tramite t, StreamWriter sw)
+    {
+        sw.WriteLine(t.IdTramite);
+        sw.WriteLine(t.ExpedienteID);
+        sw.WriteLine(t.TipoTramite);
+        sw.WriteLine(t.ContenidoTramite);
+        sw.WriteLine(t.FechaCreacion);
+        sw.WriteLine(t.FechaUltModificacion);
+        sw.WriteLine(t.UsuarioUltModificacion);
+    }
+
+    private Tramite leerTramite(StreamReader sr)
+    {
+        Tramite t = new Tramite();
+        string? id = sr.ReadLine();
+        if (!String.IsNullOrEmpty(id))
+        {
+            t.IdTramite = int.Parse(id);
+            t.ExpedienteID = int.Parse(sr.ReadLine() ?? "");
+            t.TipoTramite = Enum.Parse<EtiquetaTramite>(sr.ReadLine() ?? "");
+            t.ContenidoTramite = (sr.ReadLine() ?? "");
+            t.FechaCreacion = DateTime.Parse(sr.ReadLine() ?? "");
+            t.FechaUltModificacion = DateTime.Parse(sr.ReadLine() ?? "");
+            t.UsuarioUltModificacion = int.Parse(sr.ReadLine() ?? "");
+        }
+        else
+        {
+            throw new RepositorioException();
+        }
+        return t;
+    }
+
+    private void actualizarArchivo(List<Tramite> listaTramites)
+    {
+        try
+        {
+            if (File.Exists(_nombreArch))
+            {
+                File.Delete(_nombreArch);
+                using var sw = new StreamWriter(_nombreArch, true);
+                foreach (Tramite t in listaTramites)
+                {
+                    escribirValores(t, sw);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error al actualizar el archivo. Error " + e.Message);
+        }
+    }
+
+    private static int contadorActual()
+    {
+        using var sr = new StreamReader(_nombreArch, true);
+        int ultimoId = 0;
+        while (!sr.EndOfStream)
+        {
+            string? id = sr.ReadLine();
+            if (!String.IsNullOrEmpty(id))
+            {
+                ultimoId = Int32.Parse(id);
+                for (int i = 1; i < 7; i++)
+                {
+                    sr.ReadLine();
+                }
+            }
+        }
+        return ultimoId;
+    }
+
+    public void eliminarTramitesAsociados(int IdExpediente)
+    {
+        bool encontre = false;
+        List<Tramite> listaTramites = new List<Tramite>();
+        try
+        {
+            using var sr = new StreamReader(_nombreArch, true);
+            while (!sr.EndOfStream)
+            {
+                Tramite t = leerTramite(sr);
+                if (t.ExpedienteID != IdExpediente)
+                {
+                    listaTramites.Add(t);
+                }
+                else
+                {
+                    encontre = true;
+                }
+            }
+            if (encontre)
+            {
+                actualizarArchivo(listaTramites);
+            }
+            else
+                throw new RepositorioException("eliminarTramitesAsociados: La entidad que se intenta eliminar no existe.");
+        }
+        catch (RepositorioException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
